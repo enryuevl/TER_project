ec60d2930942abbda0552d2695d945752c0e5bed
kyo <paulandreidasco@gmail.com>
2025-10-29 12:08:08 +0800

    summary moved to dean page

--- accounts_page.py
+++ accounts_page.py
@@ -1316,7 +1316,7 @@ class AccountsDatabasePage:
 
         CTkButton(parent, text="Save", fg_color="#691612", command=submit).pack(pady=20)
 
-    
+
     def build_program_form(self, parent, mode):
         CTkLabel(parent, text="Program Form", font=("Arial", 18, "bold"),
                 text_color="#691612").pack(pady=10)
@@ -1372,8 +1372,6 @@ class AccountsDatabasePage:
                 messagebox.showerror("DB Error", str(e))
 
         CTkButton(parent, text="Save", fg_color="#691612", command=submit).pack(pady=20)
-
-
 
     def build_subject_form(self, parent, mode):
         CTkLabel(parent, text="Subject Form", font=("Arial", 18, "bold"),
